a7f7b2c33e51910ae61925885d03bc0a0fc247a6
“Vssandip” <sandipshindesr95035@gmail.com>
2023-02-12 09:46:36 +0530

    [ SANDIP ] Adding  ' Mood.cs'

---
+++ Mood.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    internal class Mood
+    {
+        public string check(string message)
+        {
+            if (message.Contains("i am sad"))
+            {
+                return "sad";
+
+
+            }
+            if (message.Contains("i am happy"))
+            {
+                return "happy";
+            }
+            return "i don't know";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine(" \"WElcome to Mood Analyzer\" ");
+            Mood a = new Mood();
+            Console.WriteLine("Enter mood");
+            string c = Console.ReadLine();
+            Console.WriteLine(a.check(c));
         }
     }
 }
