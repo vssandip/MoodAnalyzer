7f08dd71a49be08d253c72ebb8afb692a37f8869
“Vssandip” <sandipshindesr95035@gmail.com>
2023-02-12 10:00:08 +0530

    [ SANDIP ] Adding  ' Mood2.cs'

---
+++ Mood2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    internal class Mood2
+    {
+        public string message;
+
+        public Mood(string message)
+        {
+            this.message = message;
+
+        }
+
+        public string check()
+        {
+            if (message.Contains(null))
+            {
+                return "null";
+            }
+            if (message.Contains("i am sad"))
+            {
+                return "sad";
+
+
+            }
+            if (message.Contains("i am any mood"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "happy";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,14 +9,20 @@
             //Console.WriteLine("Enter mood");
             //string c = Console.ReadLine();
             //Console.WriteLine(a.check(c));
-            Mood1 a = new Mood1("i am sad");
-            Mood1 b = new Mood1("i am happy");
+            //Mood1 a = new Mood1("i am sad");
+            //Mood1 b = new Mood1("i am happy");
+
+            //Console.WriteLine("Enter mood");
+            //string c = Console.ReadLine();
+            //Console.WriteLine(a.check());
+            //Console.WriteLine(b.check());
+            Mood2 a = new Mood2("i am sad");
+            Mood2 b = new Mood2("i am happy");
 
             Console.WriteLine("Enter mood");
             string c = Console.ReadLine();
             Console.WriteLine(a.check());
             Console.WriteLine(b.check());
-
         }
     }
 }
